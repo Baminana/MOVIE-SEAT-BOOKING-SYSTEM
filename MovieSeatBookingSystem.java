4882dd6b3c1e4d5f100375e87ae89e0a5b88ce1d
Seatiel Dorado <100424151+Baminana@users.noreply.github.com>
2022-05-21 21:16:26 +0800

    Create MovieSeatBookingSystem.java

---
+++ MovieSeatBookingSystem.java
@@ -0,0 +1,16 @@
+package movie.seat.booking.system;
+/**
+ *
+ * @author Me
+ */
+public class MovieSeatBookingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+    
+}
